9510665786c947150836e1eb901eb330d4d4455e
PAO <natthachai_bubpa@cmu.ac.th>
2023-12-01 15:23:50 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x=1;
+    int E=0,O=0;
+    while(x!=0){
+        cout << "Enter an integer: ";
+        cin >> x;
+        if (x%2==0 && x!=0){
+            E++;
+        }
+        else if(x%2!=0){
+            O++;
+        }
+    }
+
+    cout << "#Even numbers = " << E << endl;
+    cout << "#Odd numbers = " << O;
     return 0;
 }
